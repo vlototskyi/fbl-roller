ca8d7c3d158c69d73f0a2d813349587765e49d62
Vitalii Lototskyi <vitalii.lototskyi@oril.co>
2023-05-12 22:26:43 +0300

    Updates

---
+++ precache-manifest.353f5398ee5c13145d537a5ef55853a7.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c181bf9302eac357a92",
+    "url": "/fbl-roller/static/css/main.d8685e79.chunk.css"
+  },
+  {
+    "revision": "5c181bf9302eac357a92",
+    "url": "/fbl-roller/static/js/main.5c181bf9.chunk.js"
+  },
+  {
+    "revision": "08eb18620c6b15d3cc19",
+    "url": "/fbl-roller/static/css/1.52542615.chunk.css"
+  },
+  {
+    "revision": "08eb18620c6b15d3cc19",
+    "url": "/fbl-roller/static/js/1.08eb1862.chunk.js"
+  },
+  {
+    "revision": "4e4d6a122e6616908ec1",
+    "url": "/fbl-roller/static/js/runtime~main.4e4d6a12.js"
+  },
+  {
+    "revision": "a499c868afa0f4fea59afe541ca0bea3",
+    "url": "/fbl-roller/static/media/crossed-swords-white.a499c868.svg"
+  },
+  {
+    "revision": "e29251ae2c34b69a7d7fdd1b5b3dea02",
+    "url": "/fbl-roller/static/media/crossed-swords-black.e29251ae.svg"
+  },
+  {
+    "revision": "255d915ae8b190064fc4cbd7d975a064",
+    "url": "/fbl-roller/static/media/swords-yellow.255d915a.svg"
+  },
+  {
+    "revision": "4eac100b932ef42cad52a311c6a8f492",
+    "url": "/fbl-roller/static/media/2-swords-yellow.4eac100b.svg"
+  },
+  {
+    "revision": "2246e7279b79aee0f1ac3bcd969aaed5",
+    "url": "/fbl-roller/static/media/2-swords-black.2246e727.svg"
+  },
+  {
+    "revision": "2406970776d17beb2968cb289f0a4038",
+    "url": "/fbl-roller/static/media/3-swords-black.24069707.svg"
+  },
+  {
+    "revision": "2b7d84b899282ea68e425121f9f15a3a",
+    "url": "/fbl-roller/static/media/2-swords-white.2b7d84b8.svg"
+  },
+  {
+    "revision": "00e6e7077e9135e4171bbca2af3f4b6c",
+    "url": "/fbl-roller/static/media/3-swords-yellow.00e6e707.svg"
+  },
+  {
+    "revision": "966abce908958f09dce929df2372a789",
+    "url": "/fbl-roller/static/media/4-swords-black.966abce9.svg"
+  },
+  {
+    "revision": "841885b2868407f78ad4069e3c49749e",
+    "url": "/fbl-roller/static/media/dread-skull-white.841885b2.svg"
+  },
+  {
+    "revision": "cc4a1d3681620a8f6cd90a04811cdddc",
+    "url": "/fbl-roller/static/media/dread-skull-black.cc4a1d36.svg"
+  },
+  {
+    "revision": "cf80075de44c8bedb2bae551c7889a33",
+    "url": "/fbl-roller/index.html"
+  }
+];
